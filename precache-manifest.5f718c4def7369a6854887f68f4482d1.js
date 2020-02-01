92a8dcc4009713bfb3ae86799bc7bea9456af3b2
Jordan Pitters <jordan_pitters@hotmail.com>
2020-02-01 00:19:03 -0500

    Updates

---
+++ precache-manifest.5f718c4def7369a6854887f68f4482d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24df42d3316c79c78081687446c40e73",
+    "url": "/JPitters/JPitters.github.io/index.html"
+  },
+  {
+    "revision": "99f4d683259f05c86622",
+    "url": "/JPitters/JPitters.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ea512d0f9b506173702e",
+    "url": "/JPitters/JPitters.github.io/static/js/2.d4472c9a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/JPitters/JPitters.github.io/static/js/2.d4472c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f4d683259f05c86622",
+    "url": "/JPitters/JPitters.github.io/static/js/main.270fcc4e.chunk.js"
+  },
+  {
+    "revision": "3d6bf8aae3cbf027fd64",
+    "url": "/JPitters/JPitters.github.io/static/js/runtime-main.1d0bcad6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JPitters/JPitters.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
