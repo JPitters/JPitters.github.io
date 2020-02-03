95be48d07a784ac80ab412336be5e4c3a7adeddb
Jordan Pitters <jordan_pitters@hotmail.com>
2020-02-03 00:12:02 -0500

    site development truly begins

--- src/js/layouts/Base/Header.js
+++ src/js/layouts/Base/Header.js
@@ -3,24 +3,43 @@ import { Nav, Navbar } from 'react-bootstrap';
 
 class Header extends Component {
     //constructor()
-    state = {  }
+    //state = {  }
 
     render() { 
         return (
-            <div className="site-header d-flex w-100 h-100 p-3 mx-auto flex-column">
-                <header className="masthead mb-auto">
-                    <div className="inner">
-                        <h3 className="masthead-brand"><a href="https://github.com/JPitters">J. Pitters</a></h3>
-                        <Nav className="nav nav-masthead justify-content-center">
-                            <a className="nav-link active" href="/">Home</a>
-                            {/*<a className="nav-link" href="#">Projects</a>
-                            <a className="nav-link" href="/about">About</a>
-                            <a className="nav-link" href="#">Blog</a>*/}
-                        </Nav>
+            <section className="site-head">
+                <div className="nav-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+                    <div className="masthead mb-auto">
+                        <div className="inner">
+                            <h3 className="masthead-brand"><a href="https://github.com/JPitters"  target="_blank" rel="noopener noreferrer">J. Pitters</a></h3>
+                            <Nav className="nav nav-masthead justify-content-center">
+                                <a className="nav-link active" href="/">Home</a>
+                                {/*<a className="nav-link" href="#">Projects</a>
+                                <a className="nav-link" href="/about">About</a>
+                                <a className="nav-link" href="#">Blog</a>*/}
+                            </Nav>
+                        </div>
                     </div>
-                </header>
-                {/* Nav goes here */}
-            </div>
+                </div>
+                
+                {/* rest goes here */}
+                <div className="header-content-container mx-auto">
+                    <div className="content-header">
+                        <h2>Jordan Pitters</h2>
+                    </div>
+                    <div className="contact-info-container row">
+                        <div className="contact-source col">
+                            LinkedIn
+                        </div>
+                        <div className="contact-source col">
+                            Github
+                        </div>
+                        <div className="contact-source col">
+                            Resume
+                        </div>
+                    </div>
+                </div>
+            </section>
         );
     }
 }
